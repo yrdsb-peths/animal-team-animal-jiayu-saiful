4d41c44801f0a9197995ebfcdbc99503ae77531a
33de9r <chenjiayu2033@gmail.com>
2025-05-13 14:08:47 -0400

    Added Label, timer & updated elephant

--- MyWorld.java
+++ MyWorld.java
@@ -1,10 +1,16 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    public int score = 0;
+    Label scoreLabel;
+    int level = 1;
     public MyWorld() {
         super(600, 400, 1);
         
         Elephant el = new Elephant();
         addObject(el, 300, 300);
+        
+        scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 50, 50);
     }
 }
