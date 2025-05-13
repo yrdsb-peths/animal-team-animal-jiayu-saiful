19ac31bcebcc65b45e4fdd721a057fe26045b72b
33de9r <chenjiayu2033@gmail.com>
2025-05-13 14:45:19 -0400

    Revert "adjusted gameplay"
    This reverts commit cd9bb142311a0d629320e9e5334bac8279577ef4.

--- Apple.java
+++ Apple.java
@@ -1,33 +1,47 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;  // Import necessary Greenfoot classes
 
 /**
- * Write a description of class Apple here.
+ * The Apple class represents a falling apple in the game.
+ * It moves downward on the screen and triggers a game over if it reaches the bottom.
  * 
- * @author Saiful Shaik 
- * @version April, 30, 2025
+ * Author: Saiful Shaik
+ * Version: April 30, 2025
  */
-
-
 public class Apple extends Actor
 {
+    // Speed at which the apple falls (not currently used in movement)
     int speed = 1;
+
+    // Constructor: sets the apple image
     public Apple() {
         setImage("images/apple.png");
     }
+
+    // Called repeatedly during the game to define the apple's behavior
     public void act()
     {
-        // Add your action code here.
+        // Get the current x and y coordinates of the apple
         int x = getX();
+<<<<<<< HEAD
         int y = getY() + speed;
         setLocation(x, y);
         
+=======
+        int y = getY() + 1; // Move apple down by 1 pixel
+        setLocation(x, y);  // Update the apple's location
+
+        // Get a reference to the current world and cast it to MyWorld
+>>>>>>> main
         MyWorld world = (MyWorld) getWorld();
+
+        // Check if the apple has reached the bottom of the screen
         if(getY() >= world.getHeight()) {
-            world.gameOver();
-            world.removeObject(this);
+            world.gameOver();         // Trigger the game over sequence
+            world.removeObject(this); // Remove the apple from the world
         }
     }
-    
+
+    // Method to set the apple's falling speed (currently unused)
     public void setSpeed(int spd) {
         speed = spd;
     }
