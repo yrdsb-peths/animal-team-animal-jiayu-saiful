67dc58a46af6ac7a17188b078bd58fe28e32204a
33de9r <chenjiayu2033@gmail.com>
2025-05-13 14:13:29 -0400

    added sound & game over

--- MyWorld.java
+++ MyWorld.java
@@ -5,12 +5,36 @@ public class MyWorld extends World {
     Label scoreLabel;
     int level = 1;
     public MyWorld() {
-        super(600, 400, 1);
+        super(600, 400, 1, false);
         
         Elephant el = new Elephant();
         addObject(el, 300, 300);
         
         scoreLabel = new Label(0, 80);
         addObject(scoreLabel, 50, 50);
+        
+        createApple();
+    }
+    
+    public void gameOver() {
+        Label gameOverLabel = new Label("Game Over", 100);
+        addObject(gameOverLabel, 300, 200);
+    }
+    
+    public void increaseScore() {
+        score++;
+        scoreLabel.setValue(score);
+        
+        if (score % 5 == 0) {
+            level += 1;
+        }
+    }
+    
+    public void createApple() {
+        Apple apple = new Apple();
+        apple.setSpeed(level);
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(apple, x, y);
     }
 }
